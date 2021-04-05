4da6ae02046f66341b28db9d87f1969d3ca6c6d6
opusheen <olga_ayo@yahoo.com>
2021-04-05 16:49:23 +0300

    first

--- java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,4 +1,32 @@
 package ru.stqa.training.selenium;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 public class MyFirstTest {
+    private WebDriver wd;
+
+    @Before
+    public void start(){
+        wd = new FirefoxDriver();
+    }
+    @Test
+    public void myFirstTest(){
+        wd.get("https://yandex.ru/");
+        wd.findElement(By.linkText("Погода")).isDisplayed();
+    }
+
+    @After
+    public void stop(){
+        wd.quit();
+        wd = null;
+    }
+
+
+
 }
